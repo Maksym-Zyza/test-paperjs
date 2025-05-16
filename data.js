8dac33cec3a4f6769e37b4d27dfae3789c7b3e60
Maksym.Zyza <93919982+maksym-zyza-telus@users.noreply.github.com>
2025-05-16 18:05:33 +0300

    add popUp code

---
+++ data.js
@@ -0,0 +1,99 @@
+export const annotations = {
+  operator: {
+    140041368577832: {
+      atype_name: "subject",
+      color_space: "rgb",
+      coords: [109, 64, 55, 77],
+      label: "operator",
+      origins: [
+        "gs://tqv-prd-ssb01-rawdata/image_readings/dp4cam01_ssb01_prd/video0/atlftpd/2025/05/12/17/c4aac411406f_video0_1747069314.000000_atlftpd_scheduled.jpg",
+      ],
+      point: "(136.0,114.0)",
+      result: null,
+      rtype: "operator",
+      threshold: 0.9585131406784058,
+    },
+    140043555318344: {
+      atype_name: "anonymous_subject",
+      color_space: "rgb",
+      coords: [109, 64, 55, 77],
+      label: "operator",
+      origins: [
+        "gs://tqv-prd-ssb01-rawdata/image_readings/dp4cam01_ssb01_prd/video0/atlftpd/2025/05/12/17/c4aac411406f_video0_1747069314.000000_atlftpd_scheduled.jpg",
+      ],
+      point: "(136.0,114.0)",
+      result: null,
+      rtype: "operator",
+      threshold: 0.9585131406784058,
+    },
+  },
+  person: {
+    140041368577352: {
+      atype_name: "subject",
+      color_space: "rgb",
+      coords: [486, 599, 125, 94],
+      label: "person",
+      origins: [
+        "gs://tqv-prd-ssb01-rawdata/image_readings/dp4cam01_ssb01_prd/video0/atlftpd/2025/05/12/17/c4aac411406f_video0_1747069314.000000_atlftpd_scheduled.jpg",
+      ],
+      point: "(548.0,646.0)",
+      result: null,
+      rtype: "person",
+      threshold: 0.9829328060150146,
+    },
+    140041371239432: {
+      atype_name: "subject",
+      color_space: "rgb",
+      coords: [319, 199, 107, 88],
+      label: "person",
+      origins: [
+        "gs://tqv-prd-ssb01-rawdata/image_readings/dp4cam01_ssb01_prd/video0/atlftpd/2025/05/12/17/c4aac411406f_video0_1747069314.000000_atlftpd_scheduled.jpg",
+      ],
+      point: "(372.0,243.0)",
+      result: null,
+      rtype: "person",
+      threshold: 0.9992940425872803,
+    },
+    140041375495176: {
+      atype_name: "anonymous_subject",
+      color_space: "rgb",
+      coords: [486, 599, 125, 94],
+      label: "person",
+      origins: [
+        "gs://tqv-prd-ssb01-rawdata/image_readings/dp4cam01_ssb01_prd/video0/atlftpd/2025/05/12/17/c4aac411406f_video0_1747069314.000000_atlftpd_scheduled.jpg",
+      ],
+      point: "(548.0,646.0)",
+      result: null,
+      rtype: "person",
+      threshold: 0.9829328060150146,
+    },
+    140041398061960: {
+      atype_name: "anonymous_subject",
+      color_space: "rgb",
+      coords: [319, 199, 107, 88],
+      label: "person",
+      origins: [
+        "gs://tqv-prd-ssb01-rawdata/image_readings/dp4cam01_ssb01_prd/video0/atlftpd/2025/05/12/17/c4aac411406f_video0_1747069314.000000_atlftpd_scheduled.jpg",
+      ],
+      point: "(372.0,243.0)",
+      result: null,
+      rtype: "person",
+      threshold: 0.9992940425872803,
+    },
+  },
+  region: {
+    140041399920904: {
+      atype_name: "region",
+      color_space: "rgb",
+      coords: [90, 60, 160, 260],
+      label: "Workstation",
+      origins: [
+        "gs://tqv-prd-ssb01-rawdata/image_readings/dp4cam01_ssb01_prd/video0/atlftpd/2025/05/12/17/c4aac411406f_video0_1747069314.000000_atlftpd_scheduled.jpg",
+      ],
+      point: "(170.0,229.0)",
+      result: null,
+      rtype: "region",
+      threshold: 0.5,
+    },
+  },
+};

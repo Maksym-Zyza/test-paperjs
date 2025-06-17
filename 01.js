06691bd2fc34f53869e9725579418cd1d5855422
Maksym.Zyza <mmziza88@gmail.com>
2025-06-17 19:18:34 +0300

    feat: add stable version

---
+++ 01.js
@@ -0,0 +1,13 @@
+<canvas
+  class="upper-canvas "
+  width="1280.0000190734863"
+  height="640.0000095367432"
+  style="position: absolute; width: 800px; height: 400px; left: 0px; top: 0px; touch-action: none; user-select: none; cursor: move;"
+></canvas>;
+
+<canvas
+  class="upper-canvas "
+  width="1280.0000190734863"
+  height="640.0000095367432"
+  style="position: absolute; width: 800px; height: 400px; left: 0px; top: 0px; touch-action: none; user-select: none; cursor: move;"
+></canvas>;
